c14e5120c89911ef97074d600c1c665ccffccf0b
KickingAnimal <dioboytje@gmail.com>
2025-08-20 22:03:35 +0200

    add baseline rpi files (from git setup repo)

---
+++ RasPi/config.py
@@ -0,0 +1,8 @@
+I2C_ADDRESS = 0x76  # BME280
+MESSAGE_TIMESPAN = 2000  # Milliseconds
+SIMULATED_DATA = True
+# Put your Device Connection String in here
+# Example: 'HostName=iothub.azure-devices.net;DeviceId=raspberry;SharedAccessKey=VGhpcyBpcyBub3QgYWN0dWFsbHkgYSBrZXkgOik='
+IOTHUB_DEVICE_CONNECTION_STRING='HostName=IoThub-aardbei.azure-devices.net;DeviceId=rasp;SharedAccessKey=9tmlCF9dIpf96rM0qi3xPS1SPjkksp9v5DzFqHrsH0k='
+
+SETUP_COMPLETED = True
